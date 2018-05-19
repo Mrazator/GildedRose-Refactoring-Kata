8c9ac0e8d422052fec68aef0ca637a0a9d9b1407
Marcel Mráz <marcelmraz@seznam.cz>
2018-05-19 20:21:30 +0200

    Added basic functionality tests

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -9,28 +9,30 @@ namespace csharp
         [TestCase("foo", 1, 1)]
         [TestCase("abcdedgh", 30, 1)]
         [TestCase("Basic Item", 150, 50)]
-        public void BasicItem_AfterUpdate_LowersBothValues(string name, int sellIn, int quality)
+        [TestCase("+5 Dexterity Vest", 10, 20)]
+        [TestCase("Elixir of the Mongoose", 5, 7)]
+        public void Item_AfterUpdate_LowersBothValues(string name, int sellIn, int quality)
         {
             var items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
             var app = new GildedRose(items);
             app.UpdateQuality();
 
-            Assert.AreEqual(name, items[0].Name);
             Assert.AreEqual(--sellIn, items[0].SellIn);
             Assert.AreEqual(--quality, items[0].Quality);
         }
 
+        //Aged Brie increases in quality twice as fast
         [TestCase("foo", -20, 2)]
         [TestCase("abcdedgh", -5, 20)]
         [TestCase("Basic Item", 0, 50)]
-        public void BasicItem_SellDatePassed_QualityDegradesTwiceAsFast(string name, int sellIn, int quality)
+        [TestCase("+5 Dexterity Vest", 0, 20)]
+        [TestCase("Elixir of the Mongoose", 0, 7)]
+        public void Item_SellDatePassed_QualityDegradesTwiceAsFast(string name, int sellIn, int quality)
         {
             var items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
             var app = new GildedRose(items);
             app.UpdateQuality();
 
-            Assert.AreEqual(name, items[0].Name);
-            Assert.AreEqual(--sellIn, items[0].SellIn);
             Assert.AreEqual(quality -2, items[0].Quality);
         }
 
@@ -38,57 +40,69 @@ namespace csharp
         [TestCase("abcdedgh", -5, 0)]
         [TestCase("Basic Item234", 10, 1)]
         [TestCase("Basic Item", 0, 2)]
+        [TestCase("+5 Dexterity Vest", 0, 0)]
+        [TestCase("Elixir of the Mongoose", 0, 0)]
         public void Item_AfterUpdate_QualityIsNeverNegative(string name, int sellIn, int quality)
         {
             var items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
             var app = new GildedRose(items);
             app.UpdateQuality();
 
-            Assert.AreEqual(name, items[0].Name);
-            Assert.AreEqual(--sellIn, items[0].SellIn);
             Assert.AreEqual(0, items[0].Quality);
         }
 
         [TestCase("Aged Brie", 1, 1)]
         [TestCase("Aged Brie", 50, 0)]
         [TestCase("Aged Brie", 0, 49)]
-        public void AgedBrie_AfterUpdate_AgedBrieQualityIncreaces(string name, int sellIn, int quality)
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 11, 30)]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 150, 49)]
+        public void SpecielItem_AfterUpdate_QualityIncreaces(string name, int sellIn, int quality)
         {
             var items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
             var app = new GildedRose(items);
             app.UpdateQuality();
 
-            Assert.AreEqual(name, items[0].Name);
-            Assert.AreEqual(--sellIn, items[0].SellIn);
             Assert.AreEqual(++quality, items[0].Quality);
         }
 
         [TestCase("Aged Brie", -10, 50)]
         [TestCase("Aged Brie", 0, 49)]
         [TestCase("Aged Brie", 20, 50)]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 1, 50)]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 6, 50)]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 11, 50)]
         public void Item_AfterUpdate_QualityIsNeverMoreThanFifty(string name, int sellIn, int quality)
         {
             var items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
             var app = new GildedRose(items);
             app.UpdateQuality();
 
-            Assert.AreEqual(name, items[0].Name);
-            Assert.AreEqual(--sellIn, items[0].SellIn);
             Assert.AreEqual(50, items[0].Quality);
         }
 
         [TestCase("Sulfuras, Hand of Ragnaros", -10, 80)]
         [TestCase("Sulfuras, Hand of Ragnaros", 50, 80)]
         [TestCase("Sulfuras, Hand of Ragnaros", 0, 80)]
-        public void Sulfuras_AfterUpdate_QualityDoesntDecrease(string name, int sellIn, int quality)
+        public void Sulfuras_AfterUpdate_QualitAndSellInStaysTheSame(string name, int sellIn, int quality)
         {
             var items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
             var app = new GildedRose(items);
             app.UpdateQuality();
 
-            Assert.AreEqual(name, items[0].Name);
             Assert.AreEqual(sellIn, items[0].SellIn);
             Assert.AreEqual(quality, items[0].Quality);
+        }
+
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 0, 40, 0)]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 5, 10, 13)]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 10, 3, 5)]
+        public void BackstagePasses_SellInValueApproaches_SpecialQualityBehaviour(string name, int sellIn, int quality, int expectedQuality)
+        {
+            var items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+
+            Assert.AreEqual(expectedQuality, items[0].Quality);
         }
     }
 }
